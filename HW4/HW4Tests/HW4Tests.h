6a8210a7ce81bb32ba9e817710782a2459824d60
Kraig McKernan <kraigsimap@gmail.com>
2013-02-11 15:05:52 -0700

    HW4 init commit

---
+++ HW4/HW4Tests/HW4Tests.h
@@ -0,0 +1,13 @@
+//
+//  HW4Tests.h
+//  HW4Tests
+//
+//  Created by Kraig McKernan on 2/11/13.
+//  Copyright (c) 2013 Kraig McKernan. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HW4Tests : SenTestCase
+
+@end
---
+++ HW4/HW4Tests/HW4Tests.m
@@ -0,0 +1,32 @@
+//
+//  HW4Tests.m
+//  HW4Tests
+//
+//  Created by Kraig McKernan on 2/11/13.
+//  Copyright (c) 2013 Kraig McKernan. All rights reserved.
+//
+
+#import "HW4Tests.h"
+
+@implementation HW4Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in HW4Tests");
+}
+
+@end
